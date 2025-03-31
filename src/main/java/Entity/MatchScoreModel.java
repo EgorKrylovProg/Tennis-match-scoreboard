6cc2f98a2f79568f22c46f71ae033e346ad6c2c3
Egor Krylov <yegor.krylov.05@gmail.com>
2025-03-31 09:29:04 +0300

    Part of the logic for winning a point by one of the players has been written

--- src/main/java/Entity/MatchScoreModel.java
+++ src/main/java/Entity/MatchScoreModel.java
@@ -3,10 +3,8 @@ package Entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.Map;
 
 @AllArgsConstructor
@@ -15,18 +13,18 @@ import java.util.Map;
 public class MatchScoreModel {
 
     private Match match;
-    private Map<TypePoints, Integer> resultsFirstPlayer;
-    private Map<TypePoints, Integer> resultsSecondPlayer;
+    private Map<TypePoints, Integer> scoreFirstPlayer;
+    private Map<TypePoints, Integer> scoreSecondPlayer;
 
     public MatchScoreModel() {
-        resultsFirstPlayer = new EnumMap<>(
+        scoreFirstPlayer = new EnumMap<>(
                 Map.of(
                         TypePoints.SET, 0,
                         TypePoints.GAME, 0,
                         TypePoints.POINTS, 0
                 )
         );
-        resultsSecondPlayer = new EnumMap<>(
+        scoreSecondPlayer = new EnumMap<>(
                 Map.of(
                         TypePoints.SET, 0,
                         TypePoints.GAME, 0,
--- src/main/java/Service/MatchScoreCalculationService.java
+++ src/main/java/Service/MatchScoreCalculationService.java
@@ -8,50 +8,65 @@ import java.util.UUID;
 
 public class MatchScoreCalculationService {
 
-    private OngoingMatchesService ongoingMatchesService = new OngoingMatchesService();
-    private Map<TypePoints, Integer> resultFirstPlayer;
-    private Map<TypePoints, Integer> resultSecondPlayer;
+    private final OngoingMatchesService ongoingMatchesService = new OngoingMatchesService();
+    private MatchScoreModel matchScore;
+    private Map<TypePoints, Integer> scoreFirstPlayer;
+    private Map<TypePoints, Integer> scoreSecondPlayer;
 
     public void updatingScore(UUID matchUuid, Integer playerId) {
-        MatchScoreModel matchScore = ongoingMatchesService.getCurrentMatch(matchUuid);
-        resultFirstPlayer = matchScore.getResultsFirstPlayer();
-        resultSecondPlayer = matchScore.getResultsSecondPlayer();
+        matchScore = ongoingMatchesService.getCurrentMatch(matchUuid);
+        scoreFirstPlayer = matchScore.getScoreFirstPlayer();
+        scoreSecondPlayer = matchScore.getScoreSecondPlayer();
 
-        if (resultFirstPlayer.get(TypePoints.GAME).equals(resultSecondPlayer.get(TypePoints.GAME))
-                && resultFirstPlayer.get(TypePoints.GAME) == 6) {
-            updatingPointsTiebreaker(matchScore, playerId);
+        if (scoreFirstPlayer.get(TypePoints.GAME).equals(scoreSecondPlayer.get(TypePoints.GAME))
+                && scoreFirstPlayer.get(TypePoints.GAME) == 6) {
+            winningTiebreakerPoint(playerId);
             checkingTiebreakerVictory();
+        } else {
+            winningPoint(playerId);
         }
 
     }
 
-    private void updatingPointsTiebreaker(MatchScoreModel matchScore, Integer playerId) {
+    private void winningTiebreakerPoint(Integer playerId) {
         if(matchScore.getMatch().getPlayerFirst().getId().intValue() == playerId) {
-            resultFirstPlayer.put(TypePoints.POINTS, resultFirstPlayer.get(TypePoints.POINTS) + 1);
+            scoreFirstPlayer.put(TypePoints.POINTS, scoreFirstPlayer.get(TypePoints.POINTS) + 1);
         } else {
-            resultSecondPlayer.put(TypePoints.POINTS, resultSecondPlayer.get(TypePoints.POINTS) + 1);
+            scoreSecondPlayer.put(TypePoints.POINTS, scoreSecondPlayer.get(TypePoints.POINTS) + 1);
         }
     }
 
     private void checkingTiebreakerVictory() {
-        if(resultFirstPlayer.get(TypePoints.POINTS) >= 7
-                && resultFirstPlayer.get(TypePoints.POINTS) - resultSecondPlayer.get(TypePoints.POINTS) == 2) {
+        if(scoreFirstPlayer.get(TypePoints.POINTS) >= 7
+                && scoreFirstPlayer.get(TypePoints.POINTS) - scoreSecondPlayer.get(TypePoints.POINTS) == 2) {
 
-            resultFirstPlayer.put(TypePoints.SET, resultFirstPlayer.get(TypePoints.SET) + 1);
-            resultFirstPlayer.put(TypePoints.GAME, 0);
-            resultFirstPlayer.put(TypePoints.POINTS, 0);
-            resultSecondPlayer.put(TypePoints.GAME, 0);
-            resultSecondPlayer.put(TypePoints.POINTS, 0);
+            scoreFirstPlayer.put(TypePoints.SET, scoreFirstPlayer.get(TypePoints.SET) + 1);
+            scoreFirstPlayer.put(TypePoints.GAME, 0);
+            scoreFirstPlayer.put(TypePoints.POINTS, 0);
+            scoreSecondPlayer.put(TypePoints.GAME, 0);
+            scoreSecondPlayer.put(TypePoints.POINTS, 0);
 
-        } else if (resultSecondPlayer.get(TypePoints.POINTS) >= 7
-                && resultSecondPlayer.get(TypePoints.POINTS) - resultFirstPlayer.get(TypePoints.POINTS) == 2) {
+        } else if (scoreSecondPlayer.get(TypePoints.POINTS) >= 7
+                && scoreSecondPlayer.get(TypePoints.POINTS) - scoreFirstPlayer.get(TypePoints.POINTS) == 2) {
 
-            resultSecondPlayer.put(TypePoints.SET, resultSecondPlayer.get(TypePoints.SET) + 1);
-            resultSecondPlayer.put(TypePoints.GAME, 0);
-            resultSecondPlayer.put(TypePoints.POINTS, 0);
-            resultFirstPlayer.put(TypePoints.GAME, 0);
-            resultFirstPlayer.put(TypePoints.POINTS, 0);
+            scoreSecondPlayer.put(TypePoints.SET, scoreSecondPlayer.get(TypePoints.SET) + 1);
+            scoreSecondPlayer.put(TypePoints.GAME, 0);
+            scoreSecondPlayer.put(TypePoints.POINTS, 0);
+            scoreFirstPlayer.put(TypePoints.GAME, 0);
+            scoreFirstPlayer.put(TypePoints.POINTS, 0);
 
+        }
+    }
+
+    private void winningPoint(Integer playerId) {
+        Integer currentPoints = scoreFirstPlayer.get(TypePoints.POINTS);
+        Integer updatedPoints = currentPoints < 30 ? scoreFirstPlayer.get(TypePoints.POINTS) + 15
+                : scoreFirstPlayer.get(TypePoints.POINTS) + 10;
+
+        if(matchScore.getMatch().getPlayerFirst().getId().intValue() == playerId) {
+            scoreFirstPlayer.put(TypePoints.POINTS, updatedPoints);
+        } else {
+            scoreSecondPlayer.put(TypePoints.POINTS, updatedPoints);
         }
     }
 
